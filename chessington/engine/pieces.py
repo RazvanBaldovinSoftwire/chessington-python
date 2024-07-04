acca5563b39c2850e106387a938b6d36facc93b4
BaldoSoft <Razvan.Baldovin@softwire.com>
2024-07-04 13:00:14 +0300

    Added move logic to the pawn

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -4,85 +4,122 @@ from chessington.engine.data import Player, Square
 from typing import TYPE_CHECKING, List
 
 if TYPE_CHECKING:
-    from chessington.engine.board import Board
+	from chessington.engine.board import Board
+
 
 class Piece(ABC):
-    """
+	"""
     An abstract base class from which all pieces inherit.
     """
 
-    def __init__(self, player: Player):
-        self.player = player
+	def __init__(self, player: Player):
+		self.player = player
 
-    @abstractmethod
-    def get_available_moves(self, board: Board) -> List[Square]:
-        """
+	@abstractmethod
+	def get_available_moves(self, board: Board) -> List[Square]:
+		"""
         Get all squares that the piece is allowed to move to.
         """
-        pass
+		pass
 
-    def move_to(self, board, new_square):
-        """
+	def move_to(self, board, new_square):
+		"""
         Move this piece to the given square on the board.
         """
-        current_square = board.find_piece(self)
-        board.move_piece(current_square, new_square)
+		current_square = board.find_piece(self)
+		board.move_piece(current_square, new_square)
 
 
 class Pawn(Piece):
-    """
+	"""
     A class representing a chess pawn.
     """
-    def get_available_moves(self, board) -> List[Square]:
-        current_square = board.find_piece(self)
-        if self.player == Player.BLACK:
-            square_in_front = Square.at(current_square.row - 1, current_square.col)
-            return [square_in_front]
-        else:
-            square_in_front = Square.at(current_square.row + 1, current_square.col)
-            return [square_in_front]
+
+	def get_available_moves(self, board) -> List[Square]:
+		current_square = board.find_piece(self)
+		available_moves = []
+
+		if self.player == Player.BLACK:
+			square_in_front = Square.at(current_square.row - 1, current_square.col)
+			if board.get_piece(square_in_front) is None:
+				available_moves.append(square_in_front)
+
+			two_squares_in_front = Square.at(current_square.row - 2, current_square.col)
+			if (board.get_piece(square_in_front) is None
+					and board.get_piece(two_squares_in_front) is None
+					and current_square.row == 6):
+				available_moves.append(two_squares_in_front)
+
+			attack_left = Square.at(current_square.row - 1, current_square.col - 1)
+			if board.get_piece(attack_left) is Player.WHITE:
+				available_moves.append(attack_left)
+
+			attack_right = Square.at(current_square.row - 1, current_square.col + 1)
+			if board.get_piece(attack_right) is Player.WHITE:
+				available_moves.append(attack_right)
+
+		else:
+			square_in_front = Square.at(current_square.row + 1, current_square.col)
+			if board.get_piece(square_in_front) is None:
+				available_moves.append(square_in_front)
+
+			two_squares_in_front = Square.at(current_square.row + 2, current_square.col)
+			if (board.get_piece(square_in_front) is None
+					and board.get_piece(two_squares_in_front) is None
+					and current_square.row == 1):
+				available_moves.append(two_squares_in_front)
+
+			attack_left = Square.at(current_square.row + 1, current_square.col - 1)
+			if board.get_piece(attack_left) is Player.BLACK:
+				available_moves.append(attack_left)
+
+			attack_right = Square.at(current_square.row + 1, current_square.col + 1)
+			if board.get_piece(attack_right) is Player.BLACK:
+				available_moves.append(attack_right)
+
+		return available_moves
 
 
 class Knight(Piece):
-    """
+	"""
     A class representing a chess knight.
     """
 
-    def get_available_moves(self, board):
-        return []
+	def get_available_moves(self, board):
+		return []
 
 
 class Bishop(Piece):
-    """
+	"""
     A class representing a chess bishop.
     """
 
-    def get_available_moves(self, board):
-        return []
+	def get_available_moves(self, board):
+		return []
 
 
 class Rook(Piece):
-    """
+	"""
     A class representing a chess rook.
     """
 
-    def get_available_moves(self, board):
-        return []
+	def get_available_moves(self, board):
+		return []
 
 
 class Queen(Piece):
-    """
+	"""
     A class representing a chess queen.
     """
 
-    def get_available_moves(self, board):
-        return []
+	def get_available_moves(self, board):
+		return []
 
 
 class King(Piece):
-    """
+	"""
     A class representing a chess king.
     """
 
-    def get_available_moves(self, board):
-        return []
+	def get_available_moves(self, board):
+		return []
